5ec9d5980a75b5f5e23a5705e02309534ae11384
rackar <27681135@qq.com>
2019-08-29 21:29:02 +0800

    init

---
+++ src/components/firstpage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      
+ 
+<div>
+     <van-image
+  round
+  width="10rem"
+  height="10rem"
+  src="https://img.yzcdn.cn/vant/cat.jpeg"
+/>
+</div>
+<div>
+小猫咪
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
